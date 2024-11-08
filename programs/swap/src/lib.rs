a1a1434ad07822e948fb7815136c2766356deaf9
jinxing <jinxing@lixiang.com>
2024-11-08 17:11:29 +0800

    swap program

---
+++ programs/swap/src/lib.rs
@@ -0,0 +1,32 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("HgVHC6dqjneUAzMRC9uCKA5VNhtj1ZseRc4hu9WpihEh");
+
+#[program]
+pub mod swap {
+    use super::*;
+
+    pub fn make_offer(
+        ctx: Context<MakeOffer>,
+        id: u64,
+        token_a_offered_amout: u64,
+        token_b_wanted_amount: u64,
+    ) -> Result<()> {
+        instructions::make_offer::send_offer_tokens_to_vault(&ctx, token_a_offered_amout)?;
+        instructions::make_offer::save_offer(ctx, id, token_b_wanted_amount)
+    }
+
+    pub fn take_offer(ctx: Context<TakeOffer>) -> Result<()> {
+        send_wanted_tokens_to_maker(&ctx)?;
+        withdraw_and_close_vault(ctx)
+    }
+}
